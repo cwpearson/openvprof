3705678960763304e8551a097c0fa167ae39bec4
Carl Pearson <carl.pearson@us.ibm.comwq>
2018-10-06 13:02:34 -0400

    add NVLink utilization counters

--- include/openvprof/nvml.hpp
+++ include/openvprof/nvml.hpp
@@ -4,69 +4,63 @@
 #include <thread>
 
 #include <boost/lockfree/queue.hpp>
+#include <nvml.h>
 
 #include "openvprof/record.hpp"
 
-
-
-
-namespace openvprof {
-namespace nvml {
-
+namespace openvprof
+{
+namespace nvml
+{
 
 void init();
 void fini();
 
-class Poller {
-public:
-    enum class Signal {
+class Poller
+{
+  public:
+    enum class Signal
+    {
         CONTINUE,
         STOP
     };
 
-  Poller(boost::lockfree::queue<Record*> *records) 
-  : records_(records), signal_(Signal::STOP) {
+    Poller(boost::lockfree::queue<Record *> *records)
+        : records_(records), signal_(Signal::STOP)
+    {
+    }
 
+    // create and start the polling thread
+    void start();
 
-  }
+    // finish the polling thread
+    void stop()
+    {
+        signal_ = Signal::STOP;
+        LOG(trace, "waiting for join");
+        thread_.join();
+    }
 
-  // create and start the polling thread
-  void start() {
-      signal_ = Signal::CONTINUE;
-      LOG(trace, "starting thread");
-      thread_ = std::thread(&Poller::run, this);
-  }
+    void pause()
+    {
+        LOG(warn, "ignoring pause");
+    }
 
-  // finish the polling thread
-  void stop() {
-      signal_ = Signal::STOP;
-      LOG(trace, "waiting for join");
-      thread_.join();
-  }
+    void resume()
+    {
+        LOG(warn, "ignoring resume");
+    }
 
-  void pause() {
-      LOG(warn, "ignoring pause");
-  }
+  private:
+    boost::lockfree::queue<Record *> *records_;
+    volatile Signal signal_;
+    std::thread thread_;
+    std::vector<nvmlDevice_t> devices_; // nvmlDevice handles by index
+    std::vector<std::vector<unsigned int>> active_nvlink_ids_; // ids of active nvlinks by device index
 
-  void resume() {
-      LOG(warn, "ignoring resume");
-  }
-
-
-
-private:
-  boost::lockfree::queue<Record*> *records_;
-  volatile Signal signal_;
-  std::thread thread_;
-
-
-  // the code executed by the polling thread
-  void run();
-
-
+    // the code executed by the polling thread
+    void run();
 };
-
-
 
 } // namespace nvml
 } // namespace openvprof
--- src/nvml.cpp
+++ src/nvml.cpp
@@ -38,36 +38,89 @@ void fini()
     nvmlShutdown();
 }
 
-void Poller::run()
+void Poller::start()
 {
-    // Query some system info and insert records
+    LOG(debug, "nvml scanning system");
 
-    // CUDA driver version
+    // CUDA driver
     {
         auto *r = new NvmlCudaDriverVersionRecord;
         nvmlSystemGetCudaDriverVersion(&r->version);
         records_->push(r);
     }
-
     // Device handles
     unsigned int num_devices;
     NVML_CHECK(nvmlDeviceGetCount(&num_devices));
     LOG(debug, "nvml got {} devices", num_devices);
-    std::vector<nvmlDevice_t> devices(num_devices);
-
-    for (unsigned int i = 0; i < devices.size(); ++i)
+    devices_.resize(num_devices);
+    
+    for (unsigned int i = 0; i < devices_.size(); ++i)
     {
-        NVML_CHECK(nvmlDeviceGetHandleByIndex(i, &devices[i]));
+        NVML_CHECK(nvmlDeviceGetHandleByIndex(i, &devices_[i]));
     }
+
+    // cache nvlinks
+    active_nvlink_ids_.resize(devices_.size());
+    for (unsigned int devIdx = 0; devIdx < devices_.size(); ++devIdx)
+    {
+        auto dev = devices_[devIdx];
+        for (size_t i = 0; i < NVML_NVLINK_MAX_LINKS; ++i)
+        {
+            nvmlEnableState_t isActive;
+            nvmlReturn_t result = nvmlDeviceGetNvLinkState(dev, i, &isActive);
+            if (NVML_ERROR_INVALID_ARGUMENT == result)
+            { // device or link is invalid
+                continue;
+            }
+            NVML_CHECK(result);
+            if (isActive == NVML_FEATURE_ENABLED)
+            {
+                LOG(debug, "NVLink {} for device {} is active", i, devIdx);
+                active_nvlink_ids_[devIdx].push_back(i);
+            }
+        }
+    }
+
+    // Reset NVLink utilization counters
+    for (unsigned int devIdx = 0; devIdx < devices_.size(); ++devIdx)
+    {
+        auto dev = devices_[devIdx];
+        for (auto linkIdx : active_nvlink_ids_[devIdx]) {
+            nvmlNvLinkUtilizationControl_t ctl;
+            NVML_CHECK(nvmlDeviceGetNvLinkUtilizationControl(dev, linkIdx, 0, &ctl));
+            ctl.units=NVML_NVLINK_COUNTER_UNIT_BYTES;
+            ctl.pktfilter=NVML_NVLINK_COUNTER_PKTFILTER_ALL;
+            NVML_CHECK(nvmlDeviceSetNvLinkUtilizationControl(dev, linkIdx, 0, &ctl, 1));
+            NVML_CHECK(nvmlDeviceGetNvLinkUtilizationControl(dev, linkIdx, 1, &ctl));
+            ctl.units=NVML_NVLINK_COUNTER_UNIT_BYTES;
+            ctl.pktfilter=NVML_NVLINK_COUNTER_PKTFILTER_ALL;
+            NVML_CHECK(nvmlDeviceSetNvLinkUtilizationControl(dev, linkIdx, 1, &ctl, 1));
+            NVML_CHECK(nvmlDeviceFreezeNvLinkUtilizationCounter (dev, linkIdx, 0, NVML_FEATURE_DISABLED)); 
+            NVML_CHECK(nvmlDeviceFreezeNvLinkUtilizationCounter (dev, linkIdx, 1, NVML_FEATURE_DISABLED)); 
+        }
+    }
+    
+
+    // start the polling thread
+    LOG(debug, "running nvml polling thread");
+    signal_ = Signal::CONTINUE;
+    thread_ = std::thread(&Poller::run, this);
+}
+
+void Poller::run()
+{
 
     // Watch the NVML values of interest
     while (signal_ == Signal::CONTINUE)
     {
-        LOG(trace, "polling nvml");
+        LOG(trace, "nvml polling thread wakeup");
 
-        for (size_t device_idx = 0; device_idx < devices.size(); ++device_idx)
+
+        // get PSTATE of all devices
+        LOG(trace, "nvml polling getting pstates");
+        for (size_t device_idx = 0; device_idx < devices_.size(); ++device_idx)
         {
-            auto dev = devices[device_idx];
+            auto dev = devices_[device_idx];
             nvmlPstates_t pState;
             NVML_CHECK(nvmlDeviceGetPerformanceState(dev, &pState));
             auto timestamp = std::chrono::high_resolution_clock::now();
@@ -78,11 +131,27 @@ void Poller::run()
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        // Get NVLink traffic on all devices
+        LOG(trace, "nvml polling nvlink counters");
+        for (size_t devIdx = 0; devIdx < devices_.size(); ++devIdx)
+        {
+            auto dev = devices_[devIdx];
+            for (auto linkIdx : active_nvlink_ids_[devIdx]) {
+                unsigned long long tx;
+                unsigned long long rx;
+                NVML_CHECK(nvmlDeviceGetNvLinkUtilizationCounter(dev, linkIdx, 0, &rx, &tx ));
+                LOG(trace, "dev:{} link:{} ctr:0 rx:{} tx:{}", devIdx, linkIdx, rx, tx);
+                NVML_CHECK(nvmlDeviceGetNvLinkUtilizationCounter(dev, linkIdx, 1, &rx, &tx ));
+                LOG(trace, "dev:{} link:{} ctr:1 rx:{} tx:{}", devIdx, linkIdx, rx, tx);
+            }
+
+
+            
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
 } // namespace nvml
 } // namespace openvprof
-
-// nvmlReturn_t nvmlDeviceGetNvLinkUtilizationCounter ( nvmlDevice_t device, unsigned int  link, unsigned int  counter, unsigned long long* rxcounter, unsigned long long* txcounter )
