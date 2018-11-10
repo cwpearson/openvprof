aef25949985e55c1e20769fad48fb465af6c289f
Carl Pearson <carl.pearson@us.ibm.comwq>
2018-11-10 14:53:55 -0500

    move most code to Tracer()

--- scripts/pynvtx.py
+++ scripts/pynvtx.py
@@ -35,61 +35,6 @@ else:
         threading.setprofile(None)
 
 
-# load the nvToolsExt library
-NVTOOLSEXT_PATHS = [
-    'libnvToolsExt.dylib',
-    'libnvToolsExt.so',
-    '/usr/local/cuda/lib/libnvToolsExt.dylib',
-    '/usr/local/cuda/lib/libnvToolsExt.so',
-]
-
-# load the nvToolsExt library
-LIBCUDART_PATHS = [
-    'libcudart.dylib',
-    'libcudart.so',
-    '/usr/local/cuda/lib/libcudart.dylib',
-    '/usr/local/cuda/lib/libcudart.so',
-]
-
-lib = None
-for path in NVTOOLSEXT_PATHS:
-    try:
-        lib = ctypes.cdll.LoadLibrary(path)
-    except OSError as e:
-        logger.debug(
-            "failed to load Nvidia Tools Extensions from {}".format(path))
-        lib = None
-    else:
-        logger.info("loaded Nvidia Tools Extensions from {}".format(path))
-        break
-
-if lib:
-    def _nvtxRangePush(s):
-        lib.nvtxRangePushA(ctypes.c_char_p(str.encode(s)))
-
-    def _nvtxRangePop():
-        lib.nvtxRangePop()
-else:
-    logger.error("couldn't load any of {}".format(NVTOOLSEXT_PATHS))
-
-    def _nvtxRangePush(_): pass
-
-    def _nvtxRangePop(): pass
-
-
-libcudart = None
-for path in LIBCUDART_PATHS:
-    try:
-        libcudart = ctypes.cdll.LoadLibrary(path)
-    except OSError as e:
-        logger.debug(
-            "failed to load CUDA Runtime from {}".format(path))
-        libcudart = None
-    else:
-        logger.info("loaded CUDA Runtime from {}".format(path))
-        break
-
-
 def get_static_class(frame):
     """return the class name of a call frame in a static method"""
     # look in the parent stack frame for a line of code that looks like a static method definition, and use that if found
@@ -152,118 +97,13 @@ def full_name(frame, module=None):
     return name
 
 
-# modcache = {}
 
-def tracefunc2(frame, event, arg, ranges=[[]]):
-    if event == "call" or event == "c_call":
-        if event == "call":
-            function_name = frame.f_code.co_name
-        else:
-            function_name = arg.__name__
-        module = inspect.getmodule(frame)
-        # _nvtxRangePush(" ")
-    elif event == "return" or event == "c_return":
-        # _nvtxRangePop()
-        pass
-
-
-def tracefunc(frame, event, arg, ranges=[[]], mode=[None]):
-
-    global DEPTH_LIMIT
-
-    # wait for the import to return
-    if mode[0]:
-        if event == "return":
-            if frame == mode[0]:
-                print("import returned")
-                mode[0] = None
-        return tracefunc
-
-    if event == "call" or event == "c_call":
-        if DEPTH_LIMIT:
-            if len(ranges[0]) > DEPTH_LIMIT:
-                return tracefunc
-
-        if event == "call":
-            # don't record call of _unsettrace (won't see exit)
-            function_name = frame.f_code.co_name
-            if function_name == "_unsettrace":
-                return tracefunc
-        else:
-            # skip builtins
-            if inspect.isbuiltin(arg):
-                return tracefunc
-            function_name = arg.__name__
-
-        # skip any imports
-        if "importlib" in frame.f_code.co_filename:
-            print("saw importlib..wait for return...")
-            mode[0] = frame
-            return tracefunc
-
-        module = inspect.getmodule(frame)
-
-        # if we have come across the init of a module, don't record ranges until it returns
-        if module and function_name == "<module>":
-            print("init of ", module, " wait for return...")
-            mode[0] = frame
-            return tracefunc
-
-        # if function_name == "<module>":
-        #     return tracefunc
-
-        # we may have defined the functions that are not part of a module, so we don't want to skip
-        # if module is None:
-        #     return tracefunc
-
-        if event == "call":
-            name = []
-            # name += [str(frame.f_code.co_filename)]
-            name += full_name(frame, module=module)
-        else:
-            name = function_full_name(arg, module=module)
-        # filename, lineno, function_name, code_context, index = inspect.getframeinfo(frame)
-        range_name = ".".join(name)
-        # print(len(ranges[0]) * " " + "PUSH", ".".join(name))
-        ranges[0].append(frame)
-        _nvtxRangePush(range_name)
-    # elif event == "c_return":
-    #     # arg is the c function object
-    #     frame_depth = depth[0]
-    #     depth[0] -= 1
-    #     if DEPTH_LIMIT and depth[0] > DEPTH_LIMIT:
-    #         return tracefunc
-    elif event == "return" or event == "c_return":
-        # don't record exit of _settrace (won't see call)
-        # if frame.f_code.co_name == "_settrace":
-        #     return tracefunc
-        if ranges[0]:
-            if ranges[0][-1] == frame:
-                _nvtxRangePop()
-                ranges[0] = ranges[0][:-1]
-                # name = full_name(frame)
-                # print(len(ranges[0]) * " " + "POP", ".".join(name))
-
-    return tracefunc
-
-
-def runctx(cmd, globals=None, locals=None):
-    if globals is None:
-        globals = {}
-    if locals is None:
-        locals = {}
-    _settrace(tracefunc)
-    try:
-        exec(cmd, globals, locals)
-    finally:
-        _unsettrace()
 
 
 class Tracer(object):
-    def __init__(self, progname, prog_argv, depth=None):
-        self.prog_name = progname
+    def __init__(self, prog_argv, depth_limit=None):
         self.prog_argv = prog_argv
-        self.depth = depth
+        self.depth_limit = depth_limit
         self.libnvtoolsext = None
         self.libcudart = None
         # load the nvToolsExt and cudart libraries
@@ -301,25 +141,132 @@ class Tracer(object):
                 logger.info(
                     "loaded CUDA Runtime from {}".format(path))
                 break
-
-        if self.libnvtoolsext:
-            def _nvtxRangePush(self, s):
-                self.libnvtoolsext.nvtxRangePushA(
-                    ctypes.c_char_p(str.encode(s)))
-
-            def _nvtxRangePop(self):
-                self.libnvtoolsext.nvtxRangePop()
-        else:
+        if not self.libnvtoolsext:
             logger.error("couldn't load any of {}".format(NVTOOLSEXT_PATHS))
+        if not self.libcudart:
+            logger.error("couldn't load any of {}".format(CUDART_PATHS))
 
-            def _nvtxRangePush(self, _): pass
+    def _nvtxRangePush(self, s): 
+        if self.libnvtoolsext:
+            logger.debug("pushing s {}".format(s))
+            self.libnvtoolsext.nvtxRangePushA(ctypes.c_char_p(str.encode(s)))
+    def _nvtxRangePop(self):
+        if self.libnvtoolsext:
+            logger.debug("popping")
+            self.libnvtoolsext.nvtxRangePop
 
-            def _nvtxRangePop(self): pass
+    def tracefunc(self, frame, event, arg, ranges=[[]], mode=[None]):
+    
+        # wait for the import to return
+        if mode[0]:
+            if event == "return":
+                if frame == mode[0]:
+                    logger.debug("import returned")
+                    mode[0] = None
+            return self.tracefunc
+    
+        if event == "call" or event == "c_call":
+            if self.depth_limit:
+                if len(ranges[0]) > self.depth_limit:
+                    return self.tracefunc
+    
+            if event == "call":
+                # don't record call of _unsettrace (won't see exit)
+                function_name = frame.f_code.co_name
+                if function_name == "_unsettrace":
+                    return self.tracefunc
+            else:
+                # skip builtins
+                if inspect.isbuiltin(arg):
+                    return self.tracefunc
+                function_name = arg.__name__
+    
+            # skip any imports
+            if "importlib" in frame.f_code.co_filename:
+                print("saw importlib..wait for return...")
+                mode[0] = frame
+                return self.tracefunc
+    
+            module = inspect.getmodule(frame)
+    
+            # if we have come across the init of a module, don't record ranges until it returns
+            if module and function_name == "<module>":
+                logger.debug("init of ", module, " wait for return...")
+                mode[0] = frame
+                return self.tracefunc
+    
+            # if function_name == "<module>":
+            #     return tracefunc
+    
+            # we may have defined the functions that are not part of a module, so we don't want to skip
+            # if module is None:
+            #     return tracefunc
+    
+            if event == "call":
+                name = []
+                # name += [str(frame.f_code.co_filename)]
+                name += full_name(frame, module=module)
+            else:
+                name = function_full_name(arg, module=module)
+            # filename, lineno, function_name, code_context, index = inspect.getframeinfo(frame)
+            range_name = ".".join(name)
+            # print(len(ranges[0]) * " " + "PUSH", ".".join(name))
+            ranges[0].append(frame)
+            self._nvtxRangePush(range_name)
+        # elif event == "c_return":
+        #     # arg is the c function object
+        #     frame_depth = depth[0]
+        #     depth[0] -= 1
+        #     if DEPTH_LIMIT and depth[0] > DEPTH_LIMIT:
+        #         return tracefunc
+        elif event == "return" or event == "c_return":
+            # don't record exit of _settrace (won't see call)
+            # if frame.f_code.co_name == "_settrace":
+            #     return tracefunc
+            if ranges[0]:
+                if ranges[0][-1] == frame:
+                    self._nvtxRangePop()
+                    ranges[0] = ranges[0][:-1]
+                    # name = full_name(frame)
+                    # print(len(ranges[0]) * " " + "POP", ".".join(name))
+    
+        return self.tracefunc
 
+    def runctx(self, cmd, globals=None, locals=None):
+        if globals is None:
+            globals = {}
+        if locals is None:
+            locals = {}
+        _settrace(self.tracefunc)
+        try:
+            exec(cmd, globals, locals)
+        finally:
+            _unsettrace()
+
+    def run(self):
+        logger.debug("Tracing python argv[:] {}".format(self.prog_argv))
+        sys.argv = self.prog_argv
+        progname = self.prog_argv[0]
+        sys.path[0] = os.path.split(progname)[0]
+        try:
+            with open(progname) as fp:
+                code = compile(fp.read(), progname, 'exec')
+                # try to emulate __main__ namespace as much as possible
+                globs = {
+                    '__file__': progname,
+                    '__name__': '__main__',
+                    '__package__': None,
+                    '__cached__': None,
+                }
+                self.runctx(code, globs, globs)
+        except IOError as err:
+            logger.critical(
+                "Cannot add nvToolsExt ranges to python file %r because: %s" % (sys.argv[0], err))
+            sys.exit(1)
+        except SystemExit:
+            pass
 
 def main():
-
-    global DEPTH_LIMIT
 
     import argparse
 
@@ -346,31 +293,10 @@ def main():
             DEPTH_LIMIT = args.depth
 
     prog_argv = args.commands
-    logger.debug("Tracing python argv[:] {}".format(prog_argv))
-    sys.argv = prog_argv
-    progname = prog_argv[0]
-    sys.path[0] = os.path.split(progname)[0]
 
-    t = Tracer(progname, prog_argv, depth=args.depth)
+    t = Tracer(prog_argv, depth_limit=args.depth)
 
-    try:
-        with open(progname) as fp:
-            code = compile(fp.read(), progname, 'exec')
-            # try to emulate __main__ namespace as much as possible
-            globs = {
-                '__file__': progname,
-                '__name__': '__main__',
-                '__package__': None,
-                '__cached__': None,
-            }
-            runctx(code, globs, globs)
-    except IOError as err:
-        logger.critical(
-            "Cannot add nvToolsExt ranges to python file %r because: %s" % (sys.argv[0], err))
-        sys.exit(1)
-    except SystemExit:
-        pass
-
+    t.run()
 
 if __name__ == '__main__':
     main()
